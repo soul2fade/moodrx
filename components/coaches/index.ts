7135e0ba16c26b2a1ccda73541f7ef12b9612688
soul2fade <32964997+soul2fade@users.noreply.github.com>
2026-05-19 12:18:22 -0700

    chore: remove dead component components/coaches/index.ts

--- components/coaches/index.ts
+++
@@ -1,5 +0,0 @@
-export { VeraFigure } from './VeraFigure';
-export { RonFigure } from './RonFigure';
-export { BrickFigure } from './BrickFigure';
-export { RickyFigure } from './RickyFigure';
-export { DerekFigure } from './DerekFigure';

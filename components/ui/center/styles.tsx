01f36529befe907905c70e9fbf6dc1fe5ae60f5e
soul2fade <32964997+soul2fade@users.noreply.github.com>
2026-05-19 12:24:25 -0700

    chore: remove unused gluestack-ui component

--- components/ui/center/styles.tsx
+++
@@ -1,7 +0,0 @@
-import { tva, isWeb } from '@gluestack-ui/utils/nativewind-utils';
-
-const baseStyle = isWeb ? 'flex flex-col relative z-0' : '';
-
-export const centerStyle = tva({
-  base: `justify-center items-center ${baseStyle}`,
-});

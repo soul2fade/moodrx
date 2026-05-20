0a55ad56ab8e94a0cb84ed638d2e4c1e15db100f
soul2fade <32964997+soul2fade@users.noreply.github.com>
2026-05-19 18:27:40 -0700

    sync: eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,25 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const noSmallFontsizeWithoutLineheight = require('./eslint-rules/no-small-fontsize-without-lineheight');
+
+const localRulesPlugin = {
+  rules: {
+    'no-small-fontsize-without-lineheight': noSmallFontsizeWithoutLineheight,
+  },
+};
 
 module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+  },
+  {
+    plugins: {
+      local: localRulesPlugin,
+    },
+    rules: {
+      'local/no-small-fontsize-without-lineheight': 'error',
+    },
   },
 ]);
